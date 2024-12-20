2473de164894262612f31b02a97eebd668f792fb
kartikmishra2004 <kartik26nov2004@gmail.com>
2024-12-20 20:26:55 +0530

    Navbar and roouter setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,20 @@
-import React, { useState } from 'react';
-import { useFirebase } from './context/firebase';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Profile from './pages/Profile.jsx'
 
 const App = () => {
-
-  const firebase = useFirebase()
-
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  });
-
-  const handleChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setFormData({
-      ...formData,
-      [name]: value,
-    })
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    firebase.signupWithEmailAndPassword(formData.email, formData.password)
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-  }
-
   return (
-    <div className="flex flex-col items-center justify-center h-screen font-sans">
-      <h2 className="mb-6 text-2xl font-semibold">User Information Form</h2>
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col gap-4 w-72"
-      >
-        <input
-          type="email"
-          name="email"
-          placeholder="example@gmail.com"
-          value={formData.email}
-          onChange={handleChange}
-          className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Enter your password"
-          value={formData.password}
-          onChange={handleChange}
-          className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-        <button
-          type="submit"
-          className="p-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        >
-          Sign Up
-        </button>
-      </form>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/auth/Signup.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { useFirebase } from '../context/firebase.jsx';
+
+const Signup = () => {
+
+  const firebase = useFirebase()
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    firebase.signupWithEmailAndPassword(formData.email, formData.password)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }
+  return (
+    <div className="flex flex-col items-center justify-center h-screen font-sans">
+      <h2 className="mb-6 text-2xl font-semibold">User Information Form</h2>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4 w-72"
+      >
+        <input
+          type="email"
+          name="email"
+          placeholder="example@gmail.com"
+          value={formData.email}
+          onChange={handleChange}
+          className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+          value={formData.password}
+          onChange={handleChange}
+          className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <button
+          type="submit"
+          className="p-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >
+          Sign Up
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default Signup
