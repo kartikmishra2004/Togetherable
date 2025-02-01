f521a94defe7acc55085b4de65f6f676d6f4aafb
kartikmishra2004 <kartik26nov2004@gmail.com>
2025-02-01 16:42:55 +0530

    bug fix

--- src/pages/Assistant.jsx
+++ src/pages/Assistant.jsx
@@ -17,7 +17,11 @@ function App() {
   const [input, setInput] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const { isScriptAdded } = useScript();
-  const { user } = useFirebase();
+  const { user, loading } = useFirebase();
+
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, [])
 
   const handleSend = async () => {
     if (!input.trim() || isLoading) return;
@@ -85,6 +89,23 @@ function App() {
       sendChatbyVoice();
     }
   }, [transcript]);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="jelly"></div>
+        <svg width="0" height="0" className="jelly-maker">
+          <defs>
+            <filter id="uib-jelly-ooze">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="6.25" result="blur"></feGaussianBlur>
+              <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="ooze"></feColorMatrix>
+              <feBlend in="SourceGraphic" in2="ooze"></feBlend>
+            </filter>
+          </defs>
+        </svg>
+      </div>
+    )
+  }
 
   if (!user) {
     return <Navigate to="/login" />;
